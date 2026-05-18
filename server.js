02166297149da1974f3f53d26a292873c214ba6a
jessielbongcales22-gif <jessielbongcales22@gmail.com>
2026-05-19 00:51:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -384,8 +384,18 @@ async function setupDatabase() {
 const distPath = path.join(__dirname, 'dist');
 if (fs.existsSync(distPath)) {
   app.use(express.static(distPath));
-  app.get('*', (req, res) => {
-    if (!req.path.startsWith('/api')) res.sendFile(path.join(distPath, 'index.html'));
+  
+  /**
+   * FIX APPLIED HERE:
+   * Express 5 requires the wildcard '*' to have a name.
+   * Changed '*' to '/*any'
+   */
+  app.get('/*any', (req, res) => {
+    if (!req.path.startsWith('/api')) {
+      res.sendFile(path.join(distPath, 'index.html'));
+    } else {
+      res.status(404).json({ error: 'API route not found' });
+    }
   });
   console.log('📁 Serving frontend from /dist');
 }
